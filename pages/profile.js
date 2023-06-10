b99c9c99936267840c190cba6b895d7c7ab7296c
Dinesh Katariya <thedineshk24@gmail.com>
2023-06-10 15:05:10 +0530

    </AuthWrapper> removed

--- pages/profile.js
+++ pages/profile.js
@@ -1,15 +1,12 @@
 import React, { useEffect } from "react";
 import "@passageidentity/passage-elements/passage-profile";
-import AuthWrapper from "../components/AuthWrapper";
 function Profile() {
     useEffect(()=>{
            require('@passageidentity/passage-auth');
          }, []);       
   return (
     <div className="mt-8">
-      <AuthWrapper>
         <passage-profile app-id="YkgyHbFAzEUIDk24xNeiDMJs"></passage-profile>
-      </AuthWrapper>
     </div>
   );
 }
