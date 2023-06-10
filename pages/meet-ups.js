b15581bdf46817d76d9cc2b0780d7c44e959eb7c
Dinesh Katariya <thedineshk24@gmail.com>
2023-06-10 15:03:04 +0530

    bug fixes

--- pages/meet-ups.js
+++ pages/meet-ups.js
@@ -1,155 +1,56 @@
-import { useEffect, useRef, useState } from 'react';
-import { useRouter } from 'next/router';
-import Peer from 'simple-peer';
+import React from 'react';
 
-const Meetup = () => {
-  const [roomId, setRoomId] = useState('');
-  const [peers, setPeers] = useState([]);
-  const userVideoRef = useRef();
-  const peersRef = useRef([]);
-  const router = useRouter();
-
-  useEffect(() => {
-    navigator.mediaDevices
-      .getUserMedia({ video: true, audio: true })
-      .then((stream) => {
-        userVideoRef.current.srcObject = stream;
-
-        const socket = new WebSocket('http://localhost:3001/'); // Replace with your WebSocket server URL
-
-        socket.onopen = () => {
-          socket.send(JSON.stringify({ action: 'join', roomId }));
-        };
-
-        socket.onmessage = (event) => {
-          const message = JSON.parse(event.data);
-
-          switch (message.action) {
-            case 'user-joined':
-              const peer = createPeer(message.userId, socket, stream);
-              peersRef.current.push({
-                peerId: message.userId,
-                peer,
-              });
-              setPeers((users) => [...users, { peerId: message.userId, peer }]);
-              break;
-
-            case 'user-left':
-              const leftPeer = peersRef.current.find(
-                (p) => p.peerId === message.userId
-              );
-              if (leftPeer) {
-                leftPeer.peer.destroy();
-              }
-              const updatedPeers = peersRef.current.filter(
-                (p) => p.peerId !== message.userId
-              );
-              peersRef.current = updatedPeers;
-              setPeers(updatedPeers);
-              break;
-
-            case 'signal':
-              const remotePeer = peersRef.current.find(
-                (p) => p.peerId === message.userId
-              );
-              if (remotePeer) {
-                remotePeer.peer.signal(message.signal);
-              }
-              break;
-
-            default:
-              break;
-          }
-        };
-
-        return () => {
-          socket.close();
-        };
-      })
-      .catch((error) => {
-        console.error('Error accessing media devices:', error);
-      });
-  }, [roomId]);
-
-  const createPeer = (userId, socket, stream) => {
-    const peer = new Peer({
-      initiator: true,
-      trickle: false,
-      stream,
-    });
-
-    peer.on('signal', (signal) => {
-      socket.send(
-        JSON.stringify({
-          action: 'signal',
-          userId,
-          signal,
-        })
-      );
-    });
-
-    return peer;
-  };
-
-  const handleJoinRoom = () => {
-    if (roomId.trim() !== '') {
-      setPeers([]);
-      router.push(`/meetup/roomId=${roomId}`);
-    }
-  };
-
+const EventCard = () => {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-      <h1 className="text-3xl font-semibold mb-6">Meetup</h1>
-      <div className="flex flex-col items-center space-y-4">
-        <div className="flex items-center space-x-2">
-          <label htmlFor="roomId" className="font-semibold">
-            Room ID:
-          </label>
-          <input
-            type="text"
-            id="roomId"
-            className="border border-gray-300 rounded px-2 py-1"
-            value={roomId}
-            onChange={(e) => setRoomId(e.target.value)}
-          />
-          <button
-            className="bg-blue-500 text-white rounded px-3 py-1"
-            onClick={handleJoinRoom}
-          >
-            Join Room
-          </button>
-        </div>
-        <div className="relative overflow-hidden w-full pb-3/4">
-          <video
-            className="absolute w-full h-full object-cover"
-            ref={userVideoRef}
-            autoPlay
-            muted
-          ></video>
-        </div>
-        {peers.map((peer) => (
-          <div
-            key={peer.peerId}
-            className="relative overflow-hidden w-full pb-3/4"
-          >
-            <video
-              className="absolute w-full h-full object-cover"
-              autoPlay
-              playsInline
-              ref={(ref) => {
-                if (ref) {
-                  peer.peer.on('stream', (stream) => {
-                    ref.srcObject = stream;
-                  });
-                }
-              }}
-            ></video>
+    <div className="flex flex-col md:flex-row bg-white shadow-lg rounded-lg overflow-hidden">
+      <div className="md:w-1/4 bg-blue-500">
+        <img
+          src="/path/to/event-image.jpg"
+          alt="Event Image"
+          className="w-full h-full object-cover"
+        />
+      </div>
+      <div className="md:w-3/4 p-4">
+        <h3 className="text-2xl font-bold mb-2">Event Title</h3>
+        <p className="text-gray-600 mb-4">
+          Event description lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
+          ullamcorper euismod sagittis.
+        </p>
+        <div className="flex items-center">
+          <div className="flex items-center mr-4">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5 mr-2 text-blue-500"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+            >
+              <path
+                fillRule="evenodd"
+                d="M10 18a8 8 0 100-16 8 8 0 000 16zm0-2a6 6 0 100-12 6 6 0 000 12zm0-3a3 3 0 110-6 3 3 0 010 6z"
+                clipRule="evenodd"
+              />
+            </svg>
+            <span className="text-gray-600">Location</span>
           </div>
-        ))}
+          <div className="flex items-center">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5 mr-2 text-blue-500"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+            >
+              <path
+                fillRule="evenodd"
+                d="M10 18a8 8 0 100-16 8 8 0 000 16zm0-2a6 6 0 100-12 6 6 0 000 12zm0-3a3 3 0 110-6 3 3 0 010 6z"
+                clipRule="evenodd"
+              />
+            </svg>
+            <span className="text-gray-600">Date & Time</span>
+          </div>
+        </div>
       </div>
     </div>
   );
 };
 
-export default Meetup;
+export default EventCard;
