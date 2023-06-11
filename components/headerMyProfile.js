b1c21acef4e29b6df5127e65971600a777995572
Dinesh Katariya <thedineshk24@gmail.com>
2023-06-11 15:01:01 +0530

    added chips for individual tech

---
+++ components/headerMyProfile.js
@@ -0,0 +1,28 @@
+import Link from "next/link";
+import React from "react";
+
+const HeaderMyProfile = () => {
+  return (
+    <header>
+      <nav className="bg-white border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800">
+        <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
+          <a href="https://flowbite.com" className="flex items-center">
+            <span className="self-center text-2xl font-bold text-indigo-600 whitespace-nowrap dark:text-white">
+              DevHub
+            </span>
+          </a>
+          <div className="flex items-center lg:order-2">
+            <Link
+              href="/profile"
+              className="text-2xl font-bold inline-block px-4 py-2 text-indigo-600 no-underline rounded-md dark:text-gray-200 hover:text-indigo-500 focus:text-indigo-500 focus:bg-indigo-100 focus:outline-none dark:focus:bg-gray-800"
+            >
+              My Profile
+            </Link>
+          </div>
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default HeaderMyProfile;
--- pages/meet-ups.js
+++ pages/meet-ups.js
@@ -1,8 +1,9 @@
-import React, { useEffect, useState } from "react";
-import { useRouter } from "next/router";
-import { createClient } from "contentful";
-import { RtcTokenBuilder, RtcRole } from "agora-access-token";
-import { useCurrentUser } from "../hooks/useCurrentUser";
+import React, {useEffect, useState} from "react";
+import {useRouter} from "next/router";
+import {createClient} from "contentful";
+import {RtcTokenBuilder, RtcRole} from "agora-access-token";
+import {useCurrentUser} from "../hooks/useCurrentUser";
+import HeaderMyProfile from "../components/headerMyProfile";
 
 const EventCard = () => {
   const router = useRouter();
@@ -24,7 +25,7 @@ const EventCard = () => {
     fetchEvent();
   }, []);
 
-  const handleJoinNow = async (eventId,channel) => {
+  const handleJoinNow = async (eventId, channel) => {
     const appID = "d3a25309e6874d26b4b57aa08756dd26";
     const appCertificate = "2e207977c08f4cc99d2c7fe6c740fdfc";
     const channelName = channel?.trim();
@@ -45,42 +46,72 @@ const EventCard = () => {
     );
 
     // Redirect to the specified URL
-    router.push(`/meetup/roomId?&token=${token}&channelName=${channelName?.trim(" ")[0]}`);
+    router.push(
+      `/meetup/roomId?&token=${token}&channelName=${channelName?.trim(" ")[0]}`
+    );
   };
 
+  console.log("eventData", eventData);
+
   return (
-    <div className="mt-2">
-      <h1 className="text-center text-indigo-600 text-3xl font-extrabold">Dev Events</h1>
-      {eventData &&
-        eventData.map((event) => (
-          <div
-            key={event.sys.id}
-            className="my-4 flex flex-col md:flex-row bg-white shadow-lg rounded-lg overflow-hidden"
-          >
-            <div className="md:w-1/4 bg-blue-500">
-              <img
-                src={event.fields.eventImage.fields.file.url}
-                alt={event.fields.eventImage.fields.title}
-                className="w-full h-full object-cover"
-              />
-            </div>
-            <div className="md:w-3/4 p-4">
-              <h3 className="text-2xl font-bold mb-2">{event.fields.eventName}</h3>
-              <p className="text-gray-600 mb-4">{event.fields.description}</p>
-              <div className="flex items-center">
-                <div className="flex items-center mr-4">
-                  <button
-                    className="bg-indigo-600 hover:bg-indigo-700 text-white font-bold py-2 px-4 rounded"
-                    onClick={() => handleJoinNow(event.sys.id,event.fields.eventName)}
-                  >
-                    Join Now
-                  </button>
+    <>
+      <HeaderMyProfile />
+      <div className="mt-2">
+        <h1 className="text-center text-indigo-600 text-3xl font-extrabold">
+          Dev Events
+        </h1>
+        {eventData &&
+          eventData.map((event) => {
+            const techArray = event?.fields?.technology
+              ?.split(",")
+              .map((tech) => tech.trim());
+            return (
+              <div
+                key={event.sys.id}
+                className="my-4 flex flex-col md:flex-row bg-white shadow-lg rounded-lg overflow-hidden"
+              >
+                <div className="md:w-1/4 bg-blue-500">
+                  <img
+                    src={event.fields.eventImage.fields.file.url}
+                    alt={event.fields.eventImage.fields.title}
+                    className="w-full h-full object-cover"
+                  />
+                </div>
+                <div className="md:w-3/4 p-4">
+                  <h3 className="text-2xl font-bold mb-2">
+                    {event.fields.eventName}
+                  </h3>
+                  <p className="text-gray-600 mb-4">
+                    {event.fields.description}
+                  </p>
+                  <div className="flex flex-wrap gap-2 my-2">
+                    {techArray.map((tech) => (
+                      <div
+                        key={tech}
+                        className="bg-gray-200 text-gray-800 py-1 px-2 rounded-full text-sm"
+                      >
+                        {tech}
+                      </div>
+                    ))}
+                  </div>
+                  <div className="flex items-center">
+                    <div className="flex items-center mr-4">
+                      <button
+                        className="bg-indigo-600 hover:bg-indigo-700 text-white font-bold py-2 px-4 rounded"
+                        onClick={() =>
+                          handleJoinNow(event.sys.id, event.fields.eventName)
+                        }
+                      >
+                        Join Now
+                      </button>
+                    </div>
+                  </div>
                 </div>
               </div>
-            </div>
-          </div>
-        ))}
-    </div>
+            );
+          })}
+      </div>
+    </>
   );
 };
 
