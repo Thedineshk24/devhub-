350406c8fe94fb88a906cc78b828259496212661
Dinesh Katariya <33573799+Thedineshk24@users.noreply.github.com>
2023-06-12 15:53:24 +0530

    Update meet-ups.js

--- pages/meet-ups.js
+++ pages/meet-ups.js
@@ -52,19 +52,27 @@ const EventCard = () => {
   };
 
   const calculateCountdown = (startDate) => {
-    const currentDate = new Date();
-    const eventDate = new Date(startDate);
+  const currentDate = new Date();
+  const eventDate = new Date(startDate);
 
-    const timeDiff = eventDate - currentDate;
-    const days = Math.floor(timeDiff / (1000 * 60 * 60 * 24));
-    const hours = Math.floor(
-      (timeDiff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
-    );
-    const minutes = Math.floor((timeDiff % (1000 * 60 * 60)) / (1000 * 60));
-    const seconds = Math.floor((timeDiff % (1000 * 60)) / 1000);
+  const timeDiff = eventDate - currentDate;
+  let days, hours, minutes, seconds;
 
-    return {days, hours, minutes, seconds};
-  };
+  if (timeDiff < 0) {
+    days = 0;
+    hours = 0;
+    minutes = 0;
+    seconds = 0;
+  } else {
+    days = Math.floor(timeDiff / (1000 * 60 * 60 * 24));
+    hours = Math.floor((timeDiff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    minutes = Math.floor((timeDiff % (1000 * 60 * 60)) / (1000 * 60));
+    seconds = Math.floor((timeDiff % (1000 * 60)) / 1000);
+  }
+
+  return { days, hours, minutes, seconds };
+};
+
 
   useEffect(() => {
     const interval = setInterval(() => {
