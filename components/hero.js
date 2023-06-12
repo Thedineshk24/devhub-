70de7a8bf14006be3246e69325207f0283a27700
Dinesh Katariya <33573799+Thedineshk24@users.noreply.github.com>
2023-06-12 15:21:49 +0530

    Update hero.js

--- components/hero.js
+++ components/hero.js
@@ -9,11 +9,11 @@ const Hero = () => {
         <div className="flex items-center w-full lg:w-1/2">
           <div className="max-w-2xl mb-8">
             <h1 className="text-4xl font-bold leading-snug tracking-tight text-gray-800 lg:text-4xl lg:leading-tight xl:text-6xl xl:leading-tight dark:text-white">
-              DevHub: connects developers with local events and communities.
+              DevHub: connects developers with online events.
             </h1>
             <p className="py-5 text-xl leading-normal text-gray-500 lg:text-xl xl:text-2xl dark:text-gray-300">
               DevHub is the ultimate website for developers who love events.
-              Find and join the best hackathons, tech talks, and more in your
+              Find and join the best tech talks, events and more in your
               city. No passwords needed -{" "}
               <a
                 target="_blank"
